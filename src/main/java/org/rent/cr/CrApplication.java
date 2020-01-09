911780af8a226a535ca4c09c70522bcb52949050
Finskiy Pavel <p.finskiy.vironit@vironit.ru>
2020-01-09 12:07:51 +0300

    init commit

---
+++ src/main/java/org/rent/cr/CrApplication.java
@@ -0,0 +1,13 @@
+package org.rent.cr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rent/cr/CrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rent.cr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
